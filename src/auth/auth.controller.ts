dba0085f138fc3215e4bea4e7d27f917f061b137
maurofluir <fluirbeats@gmail.com>
2026-02-10 23:16:55 -0300

    feat(auth): login con JWT y estructura inicial del modulo

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() body: LoginDto) {
+    const user = this.authService.validateUser(body.email, body.password);
+    return this.authService.login(user);
+  }
+}
