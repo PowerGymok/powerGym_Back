37eee72b343f2049714dd7dd4d4340e9e3f4a5be
DAlexisVillacorta <dylanvillacorta0@gmail.com>
2026-02-13 21:39:45 -0300

    Base de datos terminada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CoachModule } from './coach/coach.module';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [appConfig, databaseConfig],
     }),
 
     TypeOrmModule.forRootAsync({
