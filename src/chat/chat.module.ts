25883b36059b252ee59cd3f8f42c8b49d1b6df55
DAlexisVillacorta <dylanvillacorta0@gmail.com>
2026-02-21 20:31:41 -0300

    implementacion de sockets

---
+++ src/chat/chat.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatService } from './chat.service';
+import { ChatbotService } from './chatbot.service';
+import { ChatController } from './chat.controller';
+import { ChatGateway } from './chat.gateway';
+import { Conversation } from './entities/conversation.entity';
+import { Message } from './entities/message.entity';
+import { ChatbotFAQ } from './entities/chatbot-faq.entity';
+import { User } from '../users/users.entity';
+import { Class } from '../class/class.entity';
+
+@Module({
+  imports: [
+    // Registramos todas las entidades que usa este módulo
+    TypeOrmModule.forFeature([
+      Conversation,
+      Message,
+      ChatbotFAQ,
+      User, // Necesitamos User para verificar accesos y relaciones
+      Class, // Necesitamos Class para sugerir clases al usuario
+    ]),
+  ],
+  controllers: [ChatController],
+  providers: [
+    ChatService,
+    ChatbotService,
+    ChatGateway, // El gateway es un provider especial
+  ],
+  exports: [ChatService, ChatbotService],
+})
+export class ChatModule {}
