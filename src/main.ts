1e5be9f8225fd6106961a5a3f1ffd60e7df5d447
DAlexisVillacorta <dylanvillacorta0@gmail.com>
2026-03-10 22:53:17 -0300

    render

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Aplicación corriendo en puerto ${port}`);
   console.log(`📚 Documentación en http://localhost:${port}/api`);
 }
