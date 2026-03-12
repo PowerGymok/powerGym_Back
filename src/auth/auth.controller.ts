fee3bdc9e0e02e81493b63ff883108347b50281d
JuanMoraP <juanesmo888@gmail.com>
2026-03-12 19:09:58 -0400

    Cambio en auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -56,7 +56,7 @@ export class AuthController {
 
     const FRONT_URL = process.env.FRONT_URL;
 
-    const redirectUrl = `${FRONT_URL}?token=${encodeURIComponent(result.accessToken)}&isProfileComplete=${result.user.isProfileComplete}`;
+    const redirectUrl = `${FRONT_URL}/auth/callback?token=${encodeURIComponent(result.accessToken)}&isProfileComplete=${result.user.isProfileComplete}`;
 
     return res.redirect(redirectUrl);
   }
